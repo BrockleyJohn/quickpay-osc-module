480b6e2911aef953f1e7e288c02acc684a41a292
BrockleyJohn - John Ferguson <github@yourmontage.co.uk>
2017-10-08 17:43:59 +0100

    first cut admin hooks

---
+++ catalog/includes/hooks/admin/orders/quickpay_payment.php
@@ -0,0 +1,49 @@
+<?php
+/*
+  $Id$
+
+  add quickpay payment details to admin / orders.php using hooks
+	
+	author: John Ferguson @BrockleyJohn john@sewebsites.net
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2017 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+	// Add some things here to avoid editing application top
+	include('includes/quickpay10.php');
+	
+	// 2.3.4BS Edge compatibility
+	if (!defined('DIR_WS_CLASSES')) define('DIR_WS_CLASSES','includes/classes/');
+	if (!defined('DIR_WS_IMAGES')) define('DIR_WS_IMAGES','images/');
+	
+	include(DIR_FS_CATALOG.DIR_WS_CLASSES.'QuickpayApi.php');
+	$api = new QuickpayApi();
+	$api->setOptions(MODULE_PAYMENT_QUICKPAY_ADVANCED_USERAPIKEY);
+	
+	class hook_admin_orders_quickpay_payment {
+	
+		function listen_orderAction() {
+			if ( !class_exists('hook_admin_orders_quickpay_payment_action') ) {
+				include(DIR_FS_CATALOG . 'includes/modules/hooks/admin/orders_quickpay_payment_action.php');
+			}
+			
+			$hook = new hook_admin_orders_quickpay_payment_action();
+			
+			return $hook->execute();
+		}
+		
+		function listen_orderTab() {
+			if ( !class_exists('hook_admin_orders_quickpay_payment_tab') ) {
+				include(DIR_FS_CATALOG . 'includes/modules/hooks/admin/orders_quickpay_payment_tab.php');
+			}
+			
+			$hook = new hook_admin_orders_quickpay_payment_tab();
+			
+			return $hook->execute();
+		}
+	}
